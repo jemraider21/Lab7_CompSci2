c98f3a53d7bf1d03e14341fa9e1c59c3a51a4d06
Jared (Desktop) <jemraider21@gmail.com>
2019-11-02 05:56:02 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Employee {};
+
+/* Completed main function
+int main(){
+    vector <Employee *> VEmp;
+
+    getInput(VEmp);
+    printList(VEmp);
+    cout << "Lab 7 completed by: " << "Jared Morris" << endl;
+
+    return 0;
+}
+*/
